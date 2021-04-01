b5813ca03cea3909338a894fcf2ac65409f5b3ce
acutaia <angeloxx92@hotmail.it>
2021-04-01 12:23:38 +0200

    Fix aggregation model

--- app/models/extraction/aggregation.py
+++ app/models/extraction/aggregation.py
@@ -37,27 +37,33 @@ from ..track import AggregationType
 
 
 class AggregationExtract(OrjsonModel):
-
-    type_aggregation: Optional[AggregationType] = None
     space_aggregation: Optional[int] = None
     time_aggregation: Optional[int] = None
+    type_aggregation: Optional[AggregationType] = None
 
-    @validator("space_aggregation", always=True)
-    def type_aggregation_must_be_space_or_none(cls, v, values):
-        if values["type_aggregation"] == v or (
-            values["type_aggregation"] == AggregationType.space and v is None
-        ):
-            return v
-        raise ValueError(
-            f"type_aggregation must be {AggregationType.space} if space_aggregation is set"
-        )
+    @validator("type_aggregation", always=True)
+    def type_aggregation_must_be_coherent(cls, v, values):
+        if v == AggregationType.time:
+            if values["time_aggregation"] and values["space_aggregation"] is None:
+                return v
+            else:
+                ValueError(
+                    "type_aggregation is time so time_aggregation must be set and space_aggregation unset"
+                )
+        elif v == AggregationType.space:
+            if values["space_aggregation"] and values["time_aggregation"] is None:
+                return v
+            else:
+                ValueError(
+                    "type_aggregation is space so space_aggregation must be set and time_aggregation unset"
+                )
+        else:
+            if values["space_aggregation"] == values["time_aggregation"] and v is None:
+                return v
+            else:
+                ValueError(
+                    "type_aggregation is not set"
+                )
 
-    @validator("time_aggregation", always=True)
-    def type_aggregation_must_be_time_or_none(cls, v, values):
-        if values["type_aggregation"] == v or (
-            values["type_aggregation"] == AggregationType.time and v is None
-        ):
-            return v
-        raise ValueError(
-            f"type_aggregation must be {AggregationType.time} if time_aggregation is set"
-        )
+    def __init__(self, **data):
+        super().__init__(**data)
